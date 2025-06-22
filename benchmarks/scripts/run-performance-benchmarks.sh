afa7795c9c88802a41844ffa57b0ec9e77c210fb
黑怕 <zz307326@antgroup.com>
2025-06-22 10:31:02 +0800

    PullRequest: 130 性能测试ci
    Merge branch dev-v0.9.0604-bench_ci of git@code.alipay.com:Theta/vllm-ascend.git into dev-v0.9.0604
    https://code.alipay.com/Theta/vllm-ascend/pull_requests/130
    
    Reviewed-by: 凌煦 <wengang.cwg@antgroup.com>
    
    
    * bench ci test
    * update timeout time
    * web 显示 bench 结果
    * allow skip

--- benchmarks/scripts/run-performance-benchmarks.sh
+++ benchmarks/scripts/run-performance-benchmarks.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-BENCHMARK_SERVING_PATH=/mnt/deepseek/benchmarks
+BENCHMARK_SERVING_PATH=${1:-"/mnt/deepseek/benchmarks"}
 
 check_npus() {
   # shellcheck disable=SC2155
@@ -18,25 +18,6 @@ check_npus() {
   npu_type=$(npu-smi info | grep -E "^\| [0-9]+" | awk -F '|' '{print $2}' | awk '{$1=$1;print}' | awk '{print $2}')
 
   echo "NPU type is: $npu_type"
-}
-
-ensure_sharegpt_downloaded() {
-  local FILE="/github/home/.cache/datasets/ShareGPT_V3_unfiltered_cleaned_split.json"
-  local DIR
-  DIR=$(dirname "$FILE")
-
-  if [ ! -f "$FILE" ]; then
-    echo "$FILE not found, downloading from hf-mirror ..."
-    mkdir -p "$DIR"
-    wget -O "$FILE" https://hf-mirror.com/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
-    if [ $? -ne 0 ]; then
-      echo "Download failed!" >&2
-      return 1
-    fi
-    echo "Download completed and saved to $FILE"
-  else
-    echo "$FILE already exists."
-  fi
 }
 
 json2args() {
@@ -88,18 +69,13 @@ json2env() {
 wait_for_server() {
   # wait for vllm server to start
   # return 1 if vllm server crashes
-  timeout 300 bash -c '
+  timeout 600 bash -c '
     until curl -s -X POST localhost:8006/v1/completions >/dev/null 2>&1; do
       sleep 1
     done' && return 0 || return 1
 }
 
-get_cur_npu_id() {
-    npu-smi info -l | awk -F ':' '/NPU ID/ {print $2+0; exit}'
-}
-
 kill_npu_processes() {
-  ps -aux
   lsof -t -i:8006 | xargs -r kill -9
   pgrep python | xargs -r kill -9
   
@@ -117,92 +93,6 @@ update_json_field() {
      --arg key "$field_name" \
      '.[$key] = $value' "$json_file" > "${json_file}.tmp" && \
      mv "${json_file}.tmp" "$json_file"
-}
-
-run_latency_tests() {
-  # run latency tests using `benchmark_latency.py`
-  # $1: a json file specifying latency test cases
-
-  local latency_test_file
-  latency_test_file=$1
-
-  # Iterate over latency tests
-  jq -c '.[]' "$latency_test_file" | while read -r params; do
-    # get the test name, and append the NPU type back to it.
-    test_name=$(echo "$params" | jq -r '.test_name')
-    if [[ ! "$test_name" =~ ^latency_ ]]; then
-      echo "In latency-test.json, test_name must start with \"latency_\"."
-      exit 1
-    fi
-
-    # if TEST_SELECTOR is set, only run the test cases that match the selector
-    if [[ -n "$TEST_SELECTOR" ]] && [[ ! "$test_name" =~ $TEST_SELECTOR ]]; then
-      echo "Skip test case $test_name."
-      continue
-    fi
-
-    # get arguments
-    latency_params=$(echo "$params" | jq -r '.parameters')
-    latency_args=$(json2args "$latency_params")
-
-    latency_command="python3 vllm_benchmarks/benchmark_latency.py \
-      --output-json $RESULTS_FOLDER/${test_name}.json \
-      $latency_args"
-
-    echo "Running test case $test_name"
-    echo "Latency command: $latency_command"
-
-    # run the benchmark
-    eval "$latency_command"
-    # echo model_name to result file
-    model_name=$(echo "$latency_params" | jq -r '.model')
-    update_json_field "$RESULTS_FOLDER/${test_name}.json" "model_name" "$model_name"
-    kill_npu_processes
-
-  done
-}
-
-run_throughput_tests() {
-  # run throughput tests using `benchmark_throughput.py`
-  # $1: a json file specifying throughput test cases
-
-  local throughput_test_file
-  throughput_test_file=$1
-
-  # Iterate over throughput tests
-  jq -c '.[]' "$throughput_test_file" | while read -r params; do
-    # get the test name, and append the NPU type back to it.
-    test_name=$(echo "$params" | jq -r '.test_name')
-    if [[ ! "$test_name" =~ ^throughput_ ]]; then
-      echo "In throughput-test.json, test_name must start with \"throughput_\"."
-      exit 1
-    fi
-
-    # if TEST_SELECTOR is set, only run the test cases that match the selector
-    if [[ -n "$TEST_SELECTOR" ]] && [[ ! "$test_name" =~ $TEST_SELECTOR ]]; then
-      echo "Skip test case $test_name."
-      continue
-    fi
-
-    # get arguments
-    throughput_params=$(echo "$params" | jq -r '.parameters')
-    throughput_args=$(json2args "$throughput_params")
-
-    throughput_command="python3 vllm_benchmarks/benchmark_throughput.py \
-      --output-json $RESULTS_FOLDER/${test_name}.json \
-      $throughput_args"
-
-    echo "Running test case $test_name"
-    echo "Throughput command: $throughput_command"
-
-    # run the benchmark
-    eval "$throughput_command"
-    # echo model_name to result file
-    model_name=$(echo "$throughput_params" | jq -r '.model')
-    update_json_field "$RESULTS_FOLDER/${test_name}.json" "model_name" "$model_name"
-    kill_npu_processes
-
-  done
 }
 
 run_serving_tests() {
@@ -261,6 +151,26 @@ run_serving_tests() {
       echo ""
       echo "vllm failed to start within the timeout period."
     fi
+
+    kill -9 $server_pid
+    kill_npu_processes
+
+
+   # Rerun the server
+    echo "Running test case $test_name"
+    echo "Server command: $server_command"
+    bash -c "$server_command" &
+    server_pid=$!
+
+    # wait until the server is alive
+    if wait_for_server; then
+      echo ""
+      echo "vllm server is up and running."
+    else
+      echo ""
+      echo "vllm failed to start within the timeout period."
+    fi
+
     current_time=$(date +%m%d%H%M%S)
     new_test_name=$test_name"_"$current_time
     client_command="python vllm_benchmarks/benchmark_serving.py  \
