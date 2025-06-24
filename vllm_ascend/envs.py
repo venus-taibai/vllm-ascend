b447dd9e2a70b1347f72a1c674edeecbae73ae49
沧濯 <zhengshoujian.zsj@antgroup.com>
2025-06-24 14:09:49 +0800

    PullRequest: 183 [Refactor] Use tuple as kvcache type instead of tensor
    Merge branch zhengsj/feat/tuple-kvcache of git@code.alipay.com:Theta/vllm-ascend.git into dev-v0.9.1.0622
    https://code.alipay.com/Theta/vllm-ascend/pull_requests/183
    
    Reviewed-by: 泊营 <chenxiangfei.cxf@antgroup.com>
    
    
    * refactor: use tuple as kvcache type instead of tensor
    * feat: add VLLM_ASCEND_MLA_PA environment variable for deepseek mla decode
    * refactor: update kv_cache parameter to tuple and adjust related logic in vanilla_chunked_prefill_mla

--- vllm_ascend/envs.py
+++ vllm_ascend/envs.py
@@ -118,6 +118,11 @@ env_variables: Dict[str, Callable[[], Any]] = {
     lambda: bool(int(os.getenv('USE_OPTIMIZED_MODEL', '1'))),
     "VLLM_FUSED_EXPERTS_SEQ_SPLIT_LENGTH":
     lambda: int(os.getenv("VLLM_FUSED_EXPERTS_SEQ_SPLIT_LENGTH", "8192")),
+    # Whether to enable mla_pa for deepseek mla decode, this flag will be
+    # removed after its available torch_npu is public accessible
+    # and the mla_pa will be the default path of deepseek decode path.
+    "VLLM_ASCEND_MLA_PA":
+    lambda: int(os.getenv("VLLM_ASCEND_MLA_PA", 0)),
 }
 
 # end-env-vars-definition
