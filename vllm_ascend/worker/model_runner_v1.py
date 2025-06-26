9711182f7a82e9f61c81f232e2714892c90eb41e
皓聪 <zhanghaotong.zht@antgroup.com>
2025-06-26 10:53:26 +0800

    PullRequest: 216 [Feature]  Adaptive enhanced tracing
    Merge branch dev-v0.9.1.0622-enhanced-trace of git@code.alipay.com:Theta/vllm-ascend.git into dev-v0.9.1.0622
    https://code.alipay.com/Theta/vllm-ascend/pull_requests/216
    
    Reviewed-by: 士峻 <zongzhe.zong@antgroup.com>
    
    
    * [Feature]  Adaptive enhanced tracing

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -1556,6 +1556,9 @@ class NPUModelRunner(LoRAModelRunnerMixin):
             logprobs_tensors = sampler_output.logprobs_tensors
             logprobs_lists = logprobs_tensors.tolists() \
                 if logprobs_tensors is not None else None
+            
+            logprobs_tensors_for_trace = sampler_output.logprobs_tensors_for_trace.tolists() \
+            if sampler_output.logprobs_tensors_for_trace is not None else None
 
             # Get the valid generated tokens.
             sampled_token_ids = sampler_output.sampled_token_ids
@@ -1592,6 +1595,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
                     spec_token_ids=spec_token_ids,
                     logprobs=logprobs_lists,
                     prompt_logprobs_dict={},
+                    logprobs_tensors_for_trace=logprobs_tensors_for_trace,
                     finished_sending=finished_sending,
                     finished_recving=finished_recving,
                 )
@@ -1604,6 +1608,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
                     logprobs=logprobs_lists,
                     prompt_logprobs_dict={},
                     pooler_output=[],
+                    logprobs_tensors_for_trace=logprobs_tensors_for_trace,
                     finished_sending=finished_sending,
                     finished_recving=finished_recving,
                 )
