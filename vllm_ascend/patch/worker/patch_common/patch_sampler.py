0c4d45e5037bdbbb6d142ba27753dc8174efc1f8
徐志鹏 <xzp02264690@antgroup.com>
2025-06-27 14:25:13 +0800

    PullRequest: 224 revert 06ed7390 PullRequest: 215 [Bu...
    Merge branch revert-06ed7390-dev-v0.9.1.0622 of git@code.alipay.com:Theta/vllm-ascend.git into dev-v0.9.1.0622
    https://code.alipay.com/Theta/vllm-ascend/pull_requests/224
    
    Reviewed-by: 凌煦 <wengang.cwg@antgroup.com>
    
    
    * revert 06ed7390 PullRequest: 215 [Bu...

--- vllm_ascend/patch/worker/patch_common/patch_sampler.py
+++ vllm_ascend/patch/worker/patch_common/patch_sampler.py
@@ -11,9 +11,14 @@ def forward_npu(
     k: Optional[torch.Tensor],
     p: Optional[torch.Tensor],
 ) -> torch.Tensor:
-    logits = apply_top_k_top_p_tpu(logits, k, p)
-    probs = logits.softmax(dim=-1, dtype=torch.float32)
-    return random_sample(probs, generators)
-
+    if k is None or p is None:
+        logits = apply_top_k_top_p_tpu(logits, k, p)
+        probs = logits.softmax(dim=-1, dtype=torch.float32)
+        return random_sample(probs, generators)
+    logits_new = logits.to(torch.float16)
+    k_new = k.unsqueeze(1)
+    p_new = p.to(torch.float16).unsqueeze(1)
+    out1, out2 = torch_npu._npu_topk_topp_sampling(logits_new, k_new, p_new)
+    return out1.view(-1)
 
 TopKTopPSampler.forward_native = forward_npu
