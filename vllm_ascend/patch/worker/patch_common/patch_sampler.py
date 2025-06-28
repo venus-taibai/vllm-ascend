6e7096c7138344fa7433cac29abfddb38fa6960a
泊营 <chenxiangfei.cxf@antgroup.com>
2025-06-28 19:40:14 +0800

    PullRequest: 229 feat: set default top_k to vocal_size -1 to enable _npu_topk_topp_sampling
    Merge branch dev/default_top_k of git@code.alipay.com:Theta/vllm-ascend.git into dev-v0.9.1.0622
    https://code.alipay.com/Theta/vllm-ascend/pull_requests/229
    
    Reviewed-by: 康安 <xuxiong.xu@antgroup.com>
    
    
    * feat: set default top_k to vocal_size -1 to enable _npu_topk_topp_sampling

--- vllm_ascend/patch/worker/patch_common/patch_sampler.py
+++ vllm_ascend/patch/worker/patch_common/patch_sampler.py
@@ -3,7 +3,6 @@ import torch
 import torch_npu
 from vllm.v1.sample.ops.topk_topp_sampler import TopKTopPSampler, apply_top_k_top_p_tpu, random_sample
 
-
 def forward_npu(
     self,
     logits: torch.Tensor,
@@ -11,9 +10,15 @@ def forward_npu(
     k: Optional[torch.Tensor],
     p: Optional[torch.Tensor],
 ) -> torch.Tensor:
-    logits = apply_top_k_top_p_tpu(logits, k, p)
-    probs = logits.softmax(dim=-1, dtype=torch.float32)
-    return random_sample(probs, generators)
+    if k is None or p is None:
+        logits = apply_top_k_top_p_tpu(logits, k, p)
+        probs = logits.softmax(dim=-1, dtype=torch.float32)
+        return random_sample(probs, generators)
+    logits_new = logits.to(torch.float16)
+    k_new = k.unsqueeze(1)
+    p_new = p.to(torch.float16).unsqueeze(1)
+    out1, out2 = torch_npu._npu_topk_topp_sampling(logits_new, k_new, p_new)
+    return out1.view(-1)
 
 
 TopKTopPSampler.forward_native = forward_npu
